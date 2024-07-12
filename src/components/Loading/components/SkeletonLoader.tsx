f6997cdfe949f920bab8b4ba58656138200f386b
anthony <anthonycakins@gmail.com>
2024-07-12 15:27:54 +1000

    fixed skeleton loader

--- src/components/Loading/components/SkeletonLoader.tsx
+++ src/components/Loading/components/SkeletonLoader.tsx
@@ -5,7 +5,7 @@
 
 const SkeletonLoader: React.FC = ({}) => {
   return (
-    <div className="h-[242px] w-[225px] border border-gray-200 rounded-md">
+    <div className="h-[242px] bg-white w-[225px] border border-gray-200 rounded-md">
       <div className="h-[60px] bg-[#00000014] animate-pulse relative"></div>
       <div className="top-10 absolute  left-8 bg-white  flex justify-center items-center w-16 h-16 rounded-full">
         <div className="bg-[#00000014] z-1 h-14 w-14  animate-pulse rounded-full"></div>
